1c6a250a9ec59bca9dc8161e5e6acc1d35e1993d
Axel Bocciarelli <axelboc@gmail.com>
2015-10-17 14:13:34 +1100

    dry config

--- app.production.coffee
+++ app.production.coffee
@@ -1,47 +1,23 @@
-require('dotenv').load(silent: true)
 config = require './config'
 
-roots_config = require 'roots-config'
-roots_yaml = require 'roots-yaml'
+rootsConfig = require 'roots-config'
+yaml = require 'roots-yaml'
 contentful = require 'roots-contentful'
 browserify = require 'roots-browserify'
-css_pipeline = require 'css-pipeline'
+cssPipeline = require 'css-pipeline'
+
+# Custom configuration in production
+config.browserify.minify = true;
+config.cssPipeline.minify = true;
+config.cssPipeline.hash = true;
 
 module.exports =
-  ignores: [
-    '*.md'
-    '.git*'
-    '**/_*'
-    'views/layout.jade'
-  ]
-
+  ignores: config.ignores
   extensions: [
-    roots_config config
-    
-    roots_yaml
-      source: 'data'
-    
-    contentful
-      access_token: process.env.CONTENTFUL_KEY
-      space_id: config.spaceId
-      content_types: config.contentTypes
-    
-    browserify
-      files: 'assets/js/main.js'
-      out: 'js/build.js'
-      minify: true
-    
-    css_pipeline
-      files: 'assets/css/main.css'
-      out: 'css/build.css'
-      minify: true
-      hash: true
+    rootsConfig config
+    yaml config.yaml
+    contentful config.contentful
+    browserify config.browserify
+    cssPipeline config.cssPipeline
   ]
-
-  postcss:
-    use: [
-      require('postcss-import')(path: 'assets/css'),
-      require('postcss-nested'),
-      require('postcss-custom-properties'),
-      require('cssnano')(autoprefixer: ['> 2% in AU'])
-    ]
+  postcss: config.postcss
--- config.js
+++ config.js
@@ -1,3 +1,5 @@
+// Load environment configuration
+require('dotenv').load({ silent: true });
 
 // Load initial config from package.json
 var config = require('./package.json').config;
@@ -5,57 +7,89 @@ var config = require('./package.json').config;
 // Markdown filter for use in Jade templates
 config.marked = require('marked');
 
-// Custom Contentful content types 
-config.contentTypes = {
-  
-  albums: {
-    id: config.contentTypeIds.release,
-    filters: {
-      'fields.type': 'Studio album',
-      order: '-fields.date'
-    }
-  },
-  
-  homepages: {
-    id: config.contentTypeIds.homepage,
-    filter: {
-      'fields.isCurrent': true
-    }
-  },
-  
-  members: {
-    id: config.contentTypeIds.member,
-    filters: {
-      order: 'fields.order'
-    }
-  },
-  
-  otherReleases: {
-    id: config.contentTypeIds.release,
-    filters: {
-      'fields.type[ne]': 'Studio album',
-      order: '-fields.date'
-    }
-  },
-  
-  photoGalleries: {
-    id: config.contentTypeIds.photoGallery,
-    filters: {
-      order: 'fields.order'
-    }
-  },
-  
-  sections: {
-    id: config.contentTypeIds.section
-  },
-  
-  videoPlaylists: {
-    id: config.contentTypeIds.videoPlaylist,
-    filters: {
-      order: 'fields.order'
+// Globs for Roots to ignore
+config.ignores = [
+  '*.md',
+  '.git*',
+  '**/_*',
+  'views/layout.jade'
+];
+
+// YAML (Roots extension)
+config.yaml = {
+  source: 'data'
+};
+
+// Contentfull (Roots extension)
+config.contentful = {
+  access_token: process.env.CONTENTFUL_KEY,
+  space_id: 'v8by0dt6oh1j',
+  content_types: {
+    albums: {
+      id: config.contentTypeIds.release,
+      filters: {
+        'fields.type': 'Studio album',
+        order: '-fields.date'
+      }
+    },
+    homepages: {
+      id: config.contentTypeIds.homepage,
+      filter: {
+        'fields.isCurrent': true
+      }
+    },
+    members: {
+      id: config.contentTypeIds.member,
+      filters: {
+        order: 'fields.order'
+      }
+    },
+    otherReleases: {
+      id: config.contentTypeIds.release,
+      filters: {
+        'fields.type[ne]': 'Studio album',
+        order: '-fields.date'
+      }
+    },
+    photoGalleries: {
+      id: config.contentTypeIds.photoGallery,
+      filters: {
+        order: 'fields.order'
+      }
+    },
+    sections: {
+      id: config.contentTypeIds.section
+    },
+    videoPlaylists: {
+      id: config.contentTypeIds.videoPlaylist,
+      filters: {
+        order: 'fields.order'
+      }
     }
   }
-  
+};
+
+// Browserify (Roots extension)
+config.browserify = {
+  files: 'assets/js/main.js',
+  out: 'js/build.js'
+};
+
+// CSS pipeline (Roots extension)
+config.cssPipeline = {
+  files: 'assets/css/main.css',
+  out: 'css/build.css'
+};
+
+// PostCSS (Roots plugin)
+config.postcss = {
+  use: [
+    require('postcss-import')({ path: 'assets/css' }),
+    require('postcss-nested'),
+    require('postcss-custom-media'),
+    require('postcss-custom-properties'),
+    require('cssnano')({ autoprefixer: ['> 2% in AU'] })
+  ]
 };
 
 module.exports = config;
